3cb8cdb5b11c890b27241a18f43670a233e1a77a
Bryn McIvor <brynmcivor1@gmail.com>
2021-12-11 01:24:32 +1300

    add backdrop image to series page

--- page-components/TvSeries/index.jsx
+++ page-components/TvSeries/index.jsx
@@ -1,9 +1,26 @@
+import styles from './TvSeries.module.css';
+import Image from 'next/image';
+
+const backdropLoader = ({ src, width }) => {
+  return `https://image.tmdb.org/t/p/original${src}`;
+};
+
 export const TvSeries = ({ series }) => {
   return (
     <div>
       <h1>{series.name}</h1>
       <p>{series.tagline}</p>
       <p>{series.overview}</p>
+      <div className={styles.imageContainer}>
+        <Image
+          loader={backdropLoader}
+          src={series.backdrop_path}
+          width={16}
+          height={9}
+          layout="responsive"
+          alt={`${series.name} backdrop image`}
+        />
+      </div>
     </div>
   );
 };
