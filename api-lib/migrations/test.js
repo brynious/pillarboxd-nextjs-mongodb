f1cc498386cc2d936abd9e5e1cf4fa8d3115d6a5
Bryn McIvor <brynmcivor1@gmail.com>
2021-12-14 01:21:32 +1300

    Create test.js

---
+++ api-lib/migrations/test.js
@@ -0,0 +1,41 @@
+const { MongoClient } = require('mongodb');
+const axios = require('axios');
+require('dotenv').config();
+
+const main = async () => {
+  const client = new MongoClient(process.env.MONGODB_URI);
+
+  try {
+    // Connect to the MongoDB cluster
+    await client.connect();
+
+    const tmdb_id = 1396;
+
+    const series = await client
+      .db('production0')
+      .collection('tv_series')
+      .findOne({ tmdb_id: tmdb_id });
+    console.log(`Series: ${series._id}`);
+
+    const seasons = [];
+
+    const seasonsPromise = client
+      .db('production0')
+      .collection('tv_season')
+      .find({ series_id: series._id });
+
+    while (await seasonsPromise.hasNext()) {
+      seasons.push(await seasonsPromise.next());
+    }
+
+    seasons.forEach((season) => console.log(season.name));
+  } catch (e) {
+    console.error(e);
+  } finally {
+    // console.log('Client would close here but needs fix.');
+    console.log('Client closed.');
+    client.close();
+  }
+};
+
+main().catch(console.error);
