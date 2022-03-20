3ef73c20466dc4bc01af96fc6d511dbf666cce95
Bryn McIvor <brynmcivor1@gmail.com>
2022-03-21 01:47:20 +1300

    Add defaultlist links to user overview

--- page-components/User/WatchlistSection.jsx
+++ page-components/User/WatchlistSection.jsx
@@ -1,7 +1,12 @@
 import { Container } from '@/components/Layout';
 import styles from './WatchlistSection.module.css';
+import Link from 'next/link';
+import { useRouter } from 'next/router';
 
-const Watchlist = ({ user }) => {
+const WatchlistSection = ({ user }) => {
+  const router = useRouter();
+  const { username } = router.query;
+
   return (
     <Container className={styles.root} column alignItems="center">
       <div>
@@ -9,10 +14,25 @@ const Watchlist = ({ user }) => {
           return <div key={index}>{watchlistSeries.name}</div>;
         })}
       </div>
-      <p>{user.bio}</p>
-      <p>{user.seriesName}</p>
+      <ul>
+        <li>
+          <Link href={`/user/${username}/watchlist`}>
+            <a>Watchlist</a>
+          </Link>
+        </li>
+        <li>
+          <Link href={`/user/${username}/watching`}>
+            <a>Watching</a>
+          </Link>
+        </li>
+        <li>
+          <Link href={`/user/${username}/watched`}>
+            <a>Watched</a>
+          </Link>
+        </li>
+      </ul>
     </Container>
   );
 };
 
-export default Watchlist;
+export default WatchlistSection;
