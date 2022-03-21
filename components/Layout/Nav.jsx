d65c61f7a344827a92166e1369b47a90e5020f4a
bryn mcivor <brynmcivor1@gmail.com>
2022-03-21 22:17:45 +1300

    :icecream: Add icons to UserMenu

--- components/Layout/Nav.jsx
+++ components/Layout/Nav.jsx
@@ -76,13 +76,63 @@ const UserMenu = ({ user, mutate }) => {
               <a className={styles.item}>Profile</a>
             </Link>
             <Link passHref href={`/user/${user.username}/watchlist`}>
-              <a className={styles.item}>Watchlist</a>
+              <a className={styles.item}>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="24"
+                  height="24"
+                  viewBox="0 0 24 24"
+                  fill="none"
+                  stroke="currentColor"
+                  strokeWidth="2"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  className="feather feather-bookmark"
+                >
+                  <path d="M19 21l-7-5-7 5V5a2 2 0 0 1 2-2h10a2 2 0 0 1 2 2z"></path>
+                </svg>
+                <span>Watchlist</span>
+              </a>
             </Link>
             <Link passHref href={`/user/${user.username}/watching`}>
-              <a className={styles.item}>Watching</a>
+              <a className={styles.item}>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="24"
+                  height="24"
+                  viewBox="0 0 24 24"
+                  fill="none"
+                  stroke="currentColor"
+                  strokeWidth="2"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  className="feather feather-tv"
+                >
+                  <rect x="2" y="7" width="20" height="15" rx="2" ry="2"></rect>
+                  <polyline points="17 2 12 7 7 2"></polyline>
+                </svg>
+                <span>Watching</span>
+              </a>
             </Link>
             <Link passHref href={`/user/${user.username}/watched`}>
-              <a className={styles.item}>Watched</a>
+              <a className={styles.item}>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="24"
+                  height="24"
+                  viewBox="0 0 24 24"
+                  fill="none"
+                  stroke="currentColor"
+                  strokeWidth="2"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  className="feather feather-check-circle"
+                >
+                  <path d="M22 11.08V12a10 10 0 1 1-5.93-9.14"></path>
+                  <polyline points="22 4 12 14.01 9 11.01"></polyline>
+                </svg>
+                <span>Watched</span>
+              </a>
             </Link>
             <Link passHref href="/settings">
               <a className={styles.item}>Settings</a>
