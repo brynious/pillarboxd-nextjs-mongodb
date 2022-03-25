64c9709fbfe862604ccb4184f4aca78982b40b27
bryn mcivor <brynmcivor1@gmail.com>
2022-03-25 20:17:38 +1300

    Adjust dark background colours and sort AllSeries by popularity

--- page-components/AllSeries/index.jsx
+++ page-components/AllSeries/index.jsx
@@ -3,10 +3,20 @@ import { Wrapper, Container } from '@/components/Layout';
 import PosterImage from '@/components/PosterImage/PosterImage';
 
 export const AllSeries = ({ series }) => {
+  const orderedList = series.sort((a, b) => {
+    if (a.popularity > b.popularity) {
+      return -1;
+    }
+    if (a.popularity < b.popularity) {
+      return 1;
+    }
+    return 0;
+  });
+
   return (
     <Wrapper className={styles.root}>
       <Container flex={true} className={styles.flexContainer}>
-        {series.map((tvSeries) => {
+        {orderedList.map((tvSeries) => {
           return (
             <PosterImage
               key={tvSeries.tmdb_id}
