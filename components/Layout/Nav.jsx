04bf8d67ffbaf804fc5b7e0468870901d60adbe8
bryn mcivor <brynmcivor1@gmail.com>
2022-03-27 21:51:56 +1300

    Refactor icons

--- components/Layout/Nav.jsx
+++ components/Layout/Nav.jsx
@@ -12,6 +12,9 @@ import styles from './Nav.module.css';
 import Spacer from './Spacer';
 import Wrapper from './Wrapper';
 import Search from './Search';
+import { Ribbon } from '../Icons/Ribbon';
+import { Television } from '../Icons/Television';
+import { Check } from '../Icons/Check';
 
 const UserMenu = ({ user, mutate }) => {
   const menuRef = useRef();
@@ -77,60 +80,19 @@ const UserMenu = ({ user, mutate }) => {
             </Link>
             <Link passHref href={`/user/${user.username}/watchlist`}>
               <a className={styles.item}>
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="24"
-                  height="24"
-                  viewBox="0 0 24 24"
-                  fill="none"
-                  stroke="currentColor"
-                  strokeWidth="2"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  className="feather feather-bookmark"
-                >
-                  <path d="M19 21l-7-5-7 5V5a2 2 0 0 1 2-2h10a2 2 0 0 1 2 2z"></path>
-                </svg>
+                <Ribbon />
                 <span>Watchlist</span>
               </a>
             </Link>
             <Link passHref href={`/user/${user.username}/watching`}>
               <a className={styles.item}>
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="24"
-                  height="24"
-                  viewBox="0 0 24 24"
-                  fill="none"
-                  stroke="currentColor"
-                  strokeWidth="2"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  className="feather feather-tv"
-                >
-                  <rect x="2" y="7" width="20" height="15" rx="2" ry="2"></rect>
-                  <polyline points="17 2 12 7 7 2"></polyline>
-                </svg>
+                <Television />
                 <span>Watching</span>
               </a>
             </Link>
             <Link passHref href={`/user/${user.username}/watched`}>
               <a className={styles.item}>
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="24"
-                  height="24"
-                  viewBox="0 0 24 24"
-                  fill="none"
-                  stroke="currentColor"
-                  strokeWidth="2"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  className="feather feather-check-circle"
-                >
-                  <path d="M22 11.08V12a10 10 0 1 1-5.93-9.14"></path>
-                  <polyline points="22 4 12 14.01 9 11.01"></polyline>
-                </svg>
+                <Check />
                 <span>Watched</span>
               </a>
             </Link>
