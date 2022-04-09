15fe908e6a98c6bbf9c6f98b380bd74ac8ab5be2
bryn mcivor <brynmcivor1@gmail.com>
2022-04-09 22:18:17 +1200

    Adjust data returned for All Series page

--- api-lib/db/series.js
+++ api-lib/db/series.js
@@ -52,28 +52,7 @@ export async function getSeriesByTmdbId(db, tmdb_id) {
     .then((series) => series || null);
 }
 
-export async function getAllSeries(db) {
-  const cursor = db.collection('tv_series').find(
-    {},
-    {
-      projection: {
-        tmdb_id: 1,
-        approved_specials: 1,
-        backdrop_path: 1,
-        cast: 1,
-        name: 1,
-        overview: 1,
-        popularity: 1,
-        poster_path: 1,
-        slug: 1,
-      },
-    }
-  );
-  const allSeries = await cursor.toArray();
-  return allSeries;
-}
-
-export async function findSeries(db, before, limit = 10) {
+export async function getAllSeries(db, before, limit = 60) {
   return db
     .collection('tv_series')
     .aggregate([
@@ -84,6 +63,15 @@ export async function findSeries(db, before, limit = 10) {
       },
       { $sort: { popularity: -1 } },
       { $limit: limit },
+      {
+        $project: {
+          tmdb_id: 1,
+          name: 1,
+          popularity: 1,
+          poster_path: 1,
+          slug: 1,
+        },
+      },
     ])
     .toArray();
 }
--- pages/api/series/index.js
+++ pages/api/series/index.js
@@ -1,4 +1,4 @@
-import { findSeries } from '@/api-lib/db';
+import { getAllSeries } from '@/api-lib/db';
 import { database } from '@/api-lib/middlewares';
 import { ncOpts } from '@/api-lib/nc';
 import nc from 'next-connect';
@@ -8,7 +8,7 @@ const handler = nc(ncOpts);
 handler.use(database);
 
 handler.get(async (req, res) => {
-  const seriesList = await findSeries(
+  const seriesList = await getAllSeries(
     req.db,
     req.query.before ? parseInt(req.query.before, 10) : undefined,
     req.query.limit ? parseInt(req.query.limit, 10) : undefined
