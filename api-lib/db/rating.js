7385ebccb071b8d37931d46ed8cc3488093a787b
bryn mcivor <brynmcivor1@gmail.com>
2022-04-20 22:37:16 +1200

    Fix user season ratings
    reduced user rated seasons to 30

--- api-lib/db/rating.js
+++ api-lib/db/rating.js
@@ -152,18 +152,39 @@ export async function getAllSeriesRatedByUser(
 export async function getAllSeasonsRatedByUser(
   db,
   user_id,
-  before,
+  beforeScore,
+  beforeRatedAt,
   limit = 60
 ) {
+  // Return series that are either:
+  // A: same rating and rated earlier
+  // OR
+  // B: lower rating
+  const match = beforeScore
+    ? {
+        $match: {
+          userId: ObjectId(user_id),
+          $or: [
+            {
+              $and: [
+                { ...(beforeScore && { score: { $lte: beforeScore } }) },
+                { ...(beforeRatedAt && { ratedAt: { $lt: beforeRatedAt } }) },
+              ],
+            },
+            { ...(beforeScore && { score: { $lt: beforeScore } }) },
+          ],
+        },
+      }
+    : {
+        $match: {
+          userId: ObjectId(user_id),
+        },
+      };
+
   const ratingsCursor = await db
     .collection('season_ratings')
     .aggregate([
-      {
-        $match: {
-          userId: ObjectId(user_id),
-          ...(before && { ratedAt: { $lt: before } }),
-        },
-      },
+      { ...match },
       { $sort: { score: -1, ratedAt: -1 } },
       { $limit: limit },
       {
